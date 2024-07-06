f439b21c955c7af47482dbb97e38ef98e357e31e
Jack Brewer <jack_Brewer96@hotmail.com>
2024-07-06 11:44:39 +0100

    unreal files

---
+++ Source/TownBuilderGame/TownBuilderGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TownBuilderGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TownBuilderGame, "TownBuilderGame" );
